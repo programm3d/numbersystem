7c0244930e8f7abbd867f7ce268ce5ece4e39dd1
Pushan <pushansinha8@gmail.com>
2024-11-18 18:26:00 +0530

    The code to check numbers from an array is even or odd is ready

---
+++ index.js
@@ -0,0 +1,17 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+  //Functions to check numbers in an array is even or odd
+
+function checkEvenOdd(brr){
+    for(let i =0; i<brr.length;i++){
+        if (brr[i] % 2 == 0){
+            console.log(brr[i] + "is Even");
+        }else{
+            console.log(brr[i]+"is odd");
+        }
+    }
+}
+
+checkEvenOdd(numbers);
